087276a78c28d2752d6c0b058f2c89feee234d1f
virakkhun <rakmanoban@gmail.com>
2022-09-15 22:43:08 +0700

    chore: update container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div>
-  <div class="mx-auto w-1/2 my-10 flex justify-center items-center flex-col gap-10 lg:h-auto h-screen">
+  <div class="mx-auto lg:w-1/2 md:w-3/4 w-4/5 my-10 flex justify-center items-center flex-col gap-10 lg:h-auto h-screen">
     <img src="{HomeBanner}" alt="HomeBanner" class="hoverFilter w-3/4 mx-auto">
     <p class="text-white font-bold text-2xl hoverFilter">Welcome to Svelte Blog</p>
     <p class="text-white">
