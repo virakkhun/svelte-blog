c56728c46342eb9332d4890df69fd7df030c5b08
virakkhun <rakmanoban@gmail.com>
2022-09-20 23:37:43 +0700

    chore: update

--- src/components/layouts/header.svelte
+++ src/components/layouts/header.svelte
@@ -1,4 +1,14 @@
-<div class="z-100 sticky top-0 border-b border-white/20 backdrop-blur-md py-3 flex justify-center items-center gap-5 font-bold bg-gray-900/10 w-full">
+<script lang="ts">
+	import { onMount } from "svelte";
+
+  onMount(() => {
+    
+  })
+</script>
+
+<div 
+  class="z-100 border-b border-white/20 backdrop-blur-md py-3 flex justify-center items-center gap-5 font-bold bg-gray-900/10 w-full"
+>
   <a href="/" class="no-underline text-white filter-sm">Home</a>
   <a href="/message" class="no-underline text-white filter-sm">Message</a>  
   <a href="/blog" class="no-underline text-white filter-sm">Blog</a>  
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -14,6 +14,7 @@
   let y: number = 0
   let barIndicator = 0
   let isLoading = true
+  let currentPost: string | null = ''
 
   const fetchData = async () => {
     const url = 'https://jsonplaceholder.typicode.com/posts'
@@ -50,12 +51,31 @@
       const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       barIndicator = (winScroll / height) * 100
     })
+
+    setTimeout(() => {
+      if(typeof IntersectionObserver !== 'undefined') {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+              entry.target.classList.toggle('show', entry.isIntersecting)
+            }
+          })
+        }, {
+          threshold: 0.5
+        })
+    
+        document.querySelectorAll('article').forEach(sec => {
+          observer.observe(sec)
+        })
+      }
+    }, 2500);
+
   })
 </script>
 
 <div>
   <div class="relative">
-    <div class="h-1 bg-white w-0 sticky top-12 z-100 bg-filter" style:width={`${barIndicator}%`}></div>
+    <div class="h-0.5 bg-white w-0 sticky top-0 z-100 bg-filter" style:width={`${barIndicator}%`}></div>
     {#if isLoading}
       <div transition:fade="{{delay: 250, duration: 300}}" class="w-full justify-center items-center my-20">
         <div class="h-screen mx-auto w-20">
@@ -63,14 +83,21 @@
         </div>
       </div>
     {/if}
-    <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3 w-4/5 mx-auto my-10 ">
-      {#each data as d}
-        <div class="flex flex-col gap-2 p-2 rounded-md bg-black/20 backdrop-blur-md border border-orange/30 hoverFilter max-h-auto">
-          <p class="bg-dark text-white rounded-full p-2 w-10 h-10 text-center border border-white/10 shadow-lg">{d.id}</p>
-          <p class="font-bold text-white">{d.title}</p>
-          <p class="text-gray-300">{d.body}</p>
-        </div>      
-      {/each}
+    <div class="flex items-start gap-4 mx-auto my-10 w-4/5">
+      <div class="w-4/5 flex flex-col gap-3">
+        {#each data as d}
+          <article class="flex flex-col gap-2 p-2 rounded-md bg-black/20 backdrop-blur-md border border-orange/30 hoverFilter max-h-auto translate-x-20 article">
+            <h5 id={String(d.id)} class="bg-dark text-white rounded-full p-2 w-10 h-10 text-center border border-white/10 shadow-lg">{d.id}</h5>
+            <p class="font-bold text-white">{d.title}</p>
+            <p class="text-gray-300">{d.body}</p>
+          </article>      
+        {/each}
+      </div>
+      <div class="w-1/5 sticky top-12 grid grid-cols-5 gap-2">
+        {#each data as d}
+          <a href={`#${d.id}`} class="{currentPost === String(d.id) ? 'bg-white text-orange' : 'text-white no-underline'}">{d.id}</a>
+        {/each}
+      </div>
     </div>
     {#if y > 200}
       <button class="rounded-md border border-blue/20 backdrop-blur-md fixed bottom-4 right-4 p-3 cursor-pointer z-100" on:click={travelToTop}>Top</button>
